119281e91b61ac2e35ad39d416706373bf39cfdf
bartoszjedrzejewski <jedrus07@gmail.com>
2016-08-03 23:52:56 +0100

    first commit

---
+++ src/main/java/com/bjedrzejewski/MauritianAdventureApplication.java
@@ -0,0 +1,12 @@
+package com.bjedrzejewski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MauritianAdventureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MauritianAdventureApplication.class, args);
+	}
+}
