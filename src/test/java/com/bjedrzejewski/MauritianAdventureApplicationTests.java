2a37030b691c17298591f614f62eef1c64295f1a
bartoszjedrzejewski <jedrus07@gmail.com>
2016-12-22 12:34:48 +0000

    Fixed config to be stable. Can run from docker.

--- src/test/java/com/bjedrzejewski/MauritianAdventureApplicationTests.java
+++ src/test/java/com/bjedrzejewski/MauritianAdventureApplicationTests.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@Ignore("This is not working yet")
+@Ignore("This is not working yet - it requires redis to run")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MauritianAdventureApplicationTests {
